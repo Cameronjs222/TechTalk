0c02ed98e63fc65b25766720f154e099d16fc3e8
Jonathan Doane <johnnydoane13@gmail.com>
2023-08-15 20:28:46 -0700

    merge test at same time

---
+++ client/src/components/home.jsx
@@ -0,0 +1,23 @@
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <div>
+            <div>
+                <h1>Home</h1>
+                <a href="/">Login</a>
+                <a href="/">Sign Up</a>
+                <a href="/">Extras</a>
+                <a href="/">About Us</a>
+            </div>
+            <div>
+                <h3>Here is just a test on merging on same file editing.</h3>
+            </div>    
+        </div>
+
+    </body>
+</html>
