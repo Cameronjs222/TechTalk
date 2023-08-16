42636805a1f3e193b2f4333385ef3e5c6e19a644
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-08-15 20:05:10 -0700

    moved around routes while bug fixing

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -4,11 +4,11 @@ module.exports = (app) => {
 
     app.get('/api/users', UsersController.findAllUsers);
 
+    app.post('/api/users', UsersController.createNewUser);
+
     app.get('/api/users/:id', UsersController.findOneSingleUser);
 
     app.get('/api/users/:email', UsersController.findUserByEmail);
-
-    app.post('/api/users', UsersController.createNewUser);
 
     app.patch('/api/users/:id', UsersController.updateExistingUser);
 
