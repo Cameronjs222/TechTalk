203a53a922361c9e7a45ef3ccade9b4066e50568
Onsira <115895627+Onsira@users.noreply.github.com>
2023-08-19 13:29:10 -0700

    EditUser Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import Home from "./components/home"
 import ViewPost from "./components/viewPost"
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import LoginReg from './components/LoginReg';
+import EditUser from './components/EditUser';
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path = '/' element = {<LoginReg />}></Route>
         <Route path = '/home' element = {<Home/>}></Route>
         <Route path = '/viewPost' element = {<ViewPost/>}></Route>
+        <Route path = '/EditUser' element = {<EditUser/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
