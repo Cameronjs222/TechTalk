6bcfa661bdf05991785c1ab579b7abadd44207b8
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-08-24 11:46:46 -0700

    Added link to other users page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -149,7 +149,7 @@ const Home = () => {
             {post.slice().reverse().map(post => (
               <Link className='postLink' to={`/viewPost/${post._id}`} key={post._id}>
                 <div key={post._id} className='singlePost' style={{ border: '1px solid black', display: 'flex', justifyContent: "start", flexDirection: 'column', alignItems: "start", gap: "5px", padding: '10px' }}>
-                  <span>Today's post by {post.user_name}: {post.title}</span>
+                  <Link to={`/user/${post.user}`}><span>Today's post by {post.user_name}: {post.title}</span></Link>
                   <span>{post.content}</span>
                 </div>
               </Link>
